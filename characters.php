9c323ef075f1af402ee51526bbe9692b49a3df30
kaylee <70932714+kaygelz@users.noreply.github.com>
2021-12-07 13:05:58 -0800

    Create characters.php

---
+++ characters.php
@@ -0,0 +1,51 @@
+$homer = [
+    'first_name' => 'Homer',
+    'last_name' => 'Simpson',
+    'age' => '40',
+    'occupation' => 'Nuclear Safety Inspector',
+    'voiced_by' => 'Dan Castellaneta',
+    'image' => 'images/homer.png'
+    ]
+
+    $marge = [
+    'first_name' => 'Marge',
+    'last_name' => 'Simpson',
+    'age' => '40',
+    'occupation' => 'Homemaker',
+    'voiced_by' => 'Julie Kavner',
+    'image' => 'images/marge.png'
+    ]    
+
+    $bart = [
+    'first_name' => 'Bart',
+    'last_name' => 'Simpson',
+    'age' => '10',
+    'occupation' => 'Student',
+    'voiced_by' => 'Nancy Cartwright',
+    'image' => 'images/bart.png'
+    ]
+
+    $lisa = [
+    'first_name' => 'Lisa',
+    'last_name' => 'Simpson',
+    'age' => '8',
+    'occupation' => 'Student',
+    'voiced_by' => 'Yeardley Smith',
+    'image' => 'images/lisa.png'
+    ]
+
+    $maggie = [
+    'first_name' => 'Maggie',
+    'last_name' => 'Simpson',
+    'age' => '8',
+    'image' => 'images/maggie.png'
+    ]
+
+    $moe = [
+    'first_name' => 'Moe',
+    'last_name' => 'Szyslak',
+    'age' => '55',
+    'occupation' => 'Bartender',
+    'voiced_by' => '',
+    'image' => 'images/moe.png'
+    ]
